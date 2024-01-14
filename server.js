e740e24d4cd95a51d8ba6c38a67a5d4d086f6f2b
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-14 23:56:29 +0300

    add main application file

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express")
+
+const cors = require("cors")
+const mongoose = require("mongoose")
+const jwt = require("jsonwebtoken")
+const MONGO_URL = `mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+2.1.1`
+
+const PORT = 4001
+
+const equipment = {}
+
+
+const app = express()
+
+app.use(cors())
+
+app.get('/api/equipment', (req,res) => {
+    res.status(200).json(equipment)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
