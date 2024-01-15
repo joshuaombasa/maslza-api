7226936d07ddb9ae7337c18b39a0a5b3ac0172eb
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-15 23:58:25 +0300

    update application logic

--- server.js
+++ server.js
@@ -15,12 +15,73 @@ const app = express()
 app.use(cors())
 
 mongoose.connect(MONGO_URL)
-         .then(() => {
-            console.log(`Connected to MongoDB database`)
-         })
+    .then(() => {
+        console.log(`Connected to MongoDB database`)
+    })
+    .catch(error => {
+        console.log(error)
+    })
 
-app.get('/api/equipment', (req,res) => {
-    res.status(200).json(equipment)
+const equipmentSchema = require("./models/equipment")
+const Equipment = mongoose.model("Equipment", equipmentSchema)
+
+app.get('/api/equipment', async (req, res) => {
+    try {
+        const equipment = await Equipment.find()
+        res.status(200).json(equipment)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.get('/api/addEquipment', async (req, res) => {
+    const newEquipment = new Equipment({
+        type: "grader",
+        price: "5000",
+        name: "Bush Master",
+        imageUrl: "",
+        status: "working"
+    })
+
+    try {
+        await newEquipment.save()
+        res.status(200).json(newEquipment)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+app.get('/api/equipemt/:id', (req, res) => {
+    const id = req.params.id
+    Equipment.findById(id)
+        .then(equipment => {
+            res.status(200).json(equipment)
+        })
+        .catch(error => {
+            res.status(400).json(error)
+        })
+})
+
+app.put('/api/equipment/:id', (req,res) => {
+    const id = req.params.id
+    Equipment.findByIdAndUpdate(id, {status: "working"})
+        .then(equipment => {
+            res.status(200).json(equipment)
+        })
+        .catch(error => {
+            res.status(400).json(error)
+        })
+})
+
+app.delete('/api/equipment/:id', (req,res) => {
+    const id = req.params.id
+    Equipment.findByIdAndDelete(id, {status: "working"})
+        .then(equipment => {
+            res.status(200).json(equipment)
+        })
+        .catch(error => {
+            res.status(400).json(error)
+        })
 })
 
 app.listen(PORT, () => {
