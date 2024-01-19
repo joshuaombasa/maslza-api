b2139b850f8bfcae930571270cf93c2a81cd88ca
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-19 23:58:38 +0300

    separate yd  routes' business logic into controllers to make the structure even more modular and the code easier to read and maintain

---
+++ controllers/equipment.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose")
+
+const equipmentSchema = require("../models/equipment")
+const Equipment = mongoose.model("Equipment", equipmentSchema)
+
+
+exports.getAllEquipment = async (req, res) => {
+    try {
+        const equipment = await Equipment.find()
+        res.status(200).json(equipment)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+exports.createEquipment = async (req, res) => {
+    const newEquipment = new Equipment({
+        type: "grader",
+        price: "5000",
+        name: "Bush Master",
+        imageUrl: "",
+        status: "working"
+    })
+
+    try {
+        await newEquipment.save()
+        res.status(200).json(newEquipment)
+    } catch (error) {
+        res.status(400).json(error)
+    }
+}
+
+exports.getOneEquipment = (req, res) => {
+    const id = req.params.id
+    Equipment.findById(id)
+        .then(equipment => {
+            res.status(200).json(equipment)
+        })
+        .catch(error => {
+            res.status(400).json(error)
+        })
+}
+
+exports.modifyEquipment = (req, res) => {
+    const id = req.params.id
+    Equipment.findByIdAndUpdate(id, { status: "working" })
+        .then(equipment => {
+            res.status(200).json(equipment)
+        })
+        .catch(error => {
+            res.status(400).json(error)
+        })
+}
+
+exports.deleteEquipment = (req, res) => {
+    const id = req.params.id
+    Equipment.findByIdAndDelete(id, { status: "working" })
+        .then(equipment => {
+            res.status(200).json(equipment)
+        })
+        .catch(error => {
+            res.status(400).json(error)
+        })
+}
