892da55b6197090c7c779f9f1816289649659c7f
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-20 00:12:00 +0300

    separate yd  routes' business logic into controllers to make the structure even more modular and the code easier to read and maintain

--- routes/equipment.js
+++ routes/equipment.js
@@ -1,8 +1,7 @@
 const express = require("express")
-const mongoose = require("mongoose")
 
-const equipmentSchema = require("../models/equipment")
-const Equipment = mongoose.model("Equipment", equipmentSchema)
+
+
 const equipmentCtrl = require("../controllers/equipment")
 
 const router = express.Router()
@@ -11,38 +10,11 @@ router.get('/', equipmentCtrl.getAllEquipment)
 
 router.post('/', equipmentCtrl.createEquipment)
 
-router.get('/:id', (req, res) => {
-    const id = req.params.id
-    Equipment.findById(id)
-        .then(equipment => {
-            res.status(200).json(equipment)
-        })
-        .catch(error => {
-            res.status(400).json(error)
-        })
-})
+router.get('/:id', equipmentCtrl.getOneEquipment)
 
-router.put('/:id', (req,res) => {
-    const id = req.params.id
-    Equipment.findByIdAndUpdate(id, {status: "working"})
-        .then(equipment => {
-            res.status(200).json(equipment)
-        })
-        .catch(error => {
-            res.status(400).json(error)
-        })
-})
+router.put('/:id', equipmentCtrl.modifyEquipment)
 
-router.delete('/:id', (req,res) => {
-    const id = req.params.id
-    Equipment.findByIdAndDelete(id, {status: "working"})
-        .then(equipment => {
-            res.status(200).json(equipment)
-        })
-        .catch(error => {
-            res.status(400).json(error)
-        })
-})
+router.delete('/:id', equipmentCtrl.deleteEquipment)
 
 
 
