4786f338014c65025aa8c37d5838032dfa348a69
Joshua Ombasa <nyambegaombasa@gmail.com>
2024-01-29 21:23:41 +0300

    updated login controller

--- controllers/login.js
+++ controllers/login.js
@@ -1,15 +1,19 @@
+const mongoose = require("mongoose")
+
+
 const userSchema = require("../models/user")
-const userModel = require("../models/user")
+
 
 const User = mongoose.model("User", userSchema)
-function loginUser() {
-    (req,res) => {
-        User.find({email: email})
-             .then((user) => {
-                res.status(200).json({message: 'Login successful'})
-             })
-             .catch(error => {
-                res.status(400).json({message: 'Login unsuccessful'})
-             })
-    }
+
+exports.loginUser = (req, res) => {
+
+   const email = req.body.email
+   User.find({ email: email })
+      .then((user) => {
+         res.status(200).json({ message: 'Login successful' })
+      })
+      .catch(error => {
+         res.status(400).json({ message: 'Login unsuccessful' })
+      })
 }
